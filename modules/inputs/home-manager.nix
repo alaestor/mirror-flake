8859fefa07b55f6e77b1e3de5227e113e8b8656a
Alaestor Weissman <alaestor@0x04.cc>
2026-06-24 18:00:00 -0400

    build(flake): organize dendritic inputs
    Group Nixpkgs and Home Manager integration under a dedicated input boundary and restrict per-system outputs to the platform this flake supports.

---
+++ modules/inputs/home-manager.nix
@@ -0,0 +1,23 @@
+{ config, inputs, ... }:
+{
+  imports = [ inputs.unstable-home-manager.flakeModules.home-manager ];
+
+  flake-file.inputs = {
+    stable-home-manager = {
+      url = "github:nix-community/home-manager/release-${config.common.nixpkgs-stable-version}";
+      inputs.nixpkgs.follows = "stable-nixpkgs";
+    };
+    unstable-home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "unstable-nixpkgs";
+    };
+  };
+
+  flake.modules.nixos.stable-home-manager = {
+    imports = [ inputs.stable-home-manager.nixosModules.home-manager ];
+  };
+
+  flake.modules.nixos.unstable-home-manager = {
+    imports = [ inputs.unstable-home-manager.nixosModules.home-manager ];
+  };
+}
