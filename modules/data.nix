57ef18a240f47524e9eab5b1aaa51ac9ae429a9b
Alaestor Weissman <alaestor@0x04.cc>
2026-06-25 18:00:00 -0400

    feat(data): add external data access layer
    Provide a stable store-backed interface for paths, text, JSON, and normalized line-oriented data kept outside the import-tree module hierarchy.

---
+++ modules/data.nix
@@ -0,0 +1,40 @@
+{ lib, self, ... }:
+let
+  module-name = "data";
+
+  /**
+    flake.data provides utilites for interacting with the `data/` folder of the flake.
+  */
+
+in
+{
+  options = {
+    flake.${module-name} = lib.mkOption {
+      type = lib.types.attrsOf lib.types.unspecified;
+      default = { };
+      description = "Provides utilites for interacting with the `${module-name}/` folder of the flake.";
+    };
+
+  };
+
+  config.flake."${module-name}" = rec {
+
+    path = subpath : "${self}/${module-name}/${subpath}";
+
+    read = subpath: builtins.readFile (path subpath);
+
+    readJSON = subpath: builtins.fromJSON (read subpath);
+
+    readLines = subpath: lib.splitString "\n" (read subpath);
+
+    readNonEmptyLines = subpath: lib.filter (k: k != "") (readLines subpath);
+
+    /**
+      Common representations of frequently used data files
+    */
+    vars = {
+      sshAuthorizedKeys = readNonEmptyLines "authorized_keys";
+      textart.boykisser = path "textart/boykisser.txt";
+    };
+  };
+}
