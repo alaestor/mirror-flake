a9f6e67cd7c3ccf57e036169f47220ede917cb24
Alaestor Weissman <alaestor@0x04.cc>
2026-09-02 01:03:48 -0400

    chore: bump and alpkgs follows flake-parts

--- modules/inputs/alpkgs.nix
+++ modules/inputs/alpkgs.nix
@@ -3,5 +3,6 @@
   nucleus.inputs.alpkgs = {
     url = "git+https://git.0x04.cc/alaestor/pkgs.git";
     inputs.nixpkgs.follows = "nixpkgs";
+    inputs.flake-parts.follows = "flake-parts";
   };
 }
